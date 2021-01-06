15279844249f0dafe6ea459d1b44705776f50d5e
Andrew Savinykh <658865+AndrewSav@users.noreply.github.com>
2021-01-06 13:01:10 +1300

    Fixed errors not being logged during update

--- astroneer-update.ps1
+++ astroneer-update.ps1
@@ -1,16 +1,16 @@
 $installPath = "%installPath%"
 $scheduledTasksFolder = "%scheduledTasksFolder%"
 
-"Update started at $(Get-Date)" | Add-Content "$scheduledTasksFolder\update.log"
+"Update started at $(Get-Date)" *>> "$scheduledTasksFolder\update.log"
 Stop-Service astroneer
 
 $repo = "ricky-davis/AstroLauncher"
 $filename = "AstroLauncher.exe"
 $releasesUri = "https://api.github.com/repos/$repo/releases/latest"
 $downloadUri = ((Invoke-RestMethod -Method GET -Uri $releasesUri).assets | Where-Object name -like $filename ).browser_download_url
-Invoke-WebRequest -Uri $downloadUri -OutFile "$installPath\astroneer\AstroLauncher.exe" -UseBasicParsing | Add-Content "$scheduledTasksFolder\update.log"
+Invoke-WebRequest -Uri $downloadUri -OutFile "$installPath\astroneer\AstroLauncher.exe" -UseBasicParsing *>> "$scheduledTasksFolder\update.log"
 
-steamcmd +login anonymous +force_install_dir "$installPath\astroneer" +app_update 728470 validate +quit  | Add-Content "$scheduledTasksFolder\update.log"
+steamcmd +login anonymous +force_install_dir "$installPath\astroneer" +app_update 728470 validate +quit  *>> "$scheduledTasksFolder\update.log"
 
 Start-Service astroneer
-"Update finished at $(Get-Date)" | Add-Content "$scheduledTasksFolder\update.log"
+"Update finished at $(Get-Date)" *>> "$scheduledTasksFolder\update.log"
