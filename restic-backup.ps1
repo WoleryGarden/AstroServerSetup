bac26a61c755505a12edaa35bd27aad74578ffe1
Andrew Savinykh <658865+AndrewSav@users.noreply.github.com>
2021-01-06 13:00:02 +1300

    Fixed errors not being logged during back up

--- restic-backup.ps1
+++ restic-backup.ps1
@@ -1,12 +1,13 @@
 $env:AWS_ACCESS_KEY_ID="%AWS_ACCESS_KEY_ID%"
 $env:AWS_SECRET_ACCESS_KEY="%AWS_SECRET_ACCESS_KEY%"
 $env:RESTIC_PASSWORD="%RESTIC_PASSWORD%"
+$env:RESTIC_REPOSITORY="%backupLocation%"
 
 $scheduledTasksFolder = "%scheduledTasksFolder%"
 $installPath = "%installPath%"
-$backupLocation = "%backupLocation%"
 
-"Backup started at $(Get-Date)" | Add-Content "$scheduledTasksFolder\backup.log"
-restic -r $backupLocation backup "$installPath\astroneer\Launcher.ini" "$installPath\astroneer\Astro\Saved\Config\WindowsServer" "$installPath\astroneer\Astro\Saved\SaveGames" | Add-Content "$scheduledTasksFolder\backup.log"
-restic -r $backupLocation forget --keep-last 10 --keep-hourly 48 --keep-daily 7 --keep-weekly 5 --keep-monthly 6 | Add-Content "$scheduledTasksFolder\backup.log"
-"Backup finished at $(Get-Date)" | Add-Content "$scheduledTasksFolder\backup.log"
+"Backup started at $(Get-Date)" *>> "$scheduledTasksFolder\backup.log"
+restic backup "$installPath\astroneer\Launcher.ini" "$installPath\astroneer\Astro\Saved\Config\WindowsServer" "$installPath\astroneer\Astro\Saved\SaveGames" *>> "$scheduledTasksFolder\backup.log"
+restic forget --keep-last 10 --keep-hourly 48 --keep-daily 7 --keep-weekly 5 --keep-monthly 6 *>> "$scheduledTasksFolder\backup.log"
+restic prune *>> "$scheduledTasksFolder\backup.log"
+"Backup finished at $(Get-Date)" *>> "$scheduledTasksFolder\backup.log"
